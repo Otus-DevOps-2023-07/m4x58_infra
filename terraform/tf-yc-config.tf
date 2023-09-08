64ad5d8c0d0900844a3b9095386c37494c711482
ubuntu <ubuntu@ubuntu.ubuntu>
2023-09-08 15:51:48 +0300

    terraform

---
+++ terraform/tf-yc-config.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
+#provider "yandex" {
+# token     = "y0_AgAAAAADiYdUAATuwQAAAADrR12evHn_SAXcQ8uebbr-LBXDN2x3mnQ"
+#cloud_id  = "b1gfnnr7dn30ornsm35s"
+#folder_id = "b1g5q2lccug3i0j92ksh"
+#zone      = "ru-central1-a"
+#}
