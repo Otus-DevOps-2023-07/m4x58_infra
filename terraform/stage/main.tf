19e529b7d34a2babdf2473d13ed2e12bd2d3a1e4
m4x58 <mvidineev@yandex.ru>
2023-09-21 09:18:16 +0300

    Update main.tf

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -17,6 +17,12 @@ source = "../modules/app"
 public_key_path = var.public_key_path
 app_disk_image = var.app_disk_image
 subnet_id = var.subnet_id
+service_account_key_file = var.service_account_key_file
+folder_id                = var.folder_id
+image_id                 = var.image_id
+cloud_id                 = var.cloud_id
+private_key_path         = var.private_key_path
+}
 }
 module "db" {
 source = "../modules/db"
