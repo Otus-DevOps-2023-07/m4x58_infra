ac349d851ae68f3a2e95e492398c06824cc7f24e
m4x58 <mvidineev@yandex.ru>
2023-09-27 08:35:06 +0300

    Update main.tf

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,10 +1,10 @@
 #terraform {
- # required_providers {
- #   yandex = {
-  #    source  = "yandex-cloud/yandex"
- #     version = "0.98.0"
- #   }
+#  required_providers {
+#   yandex = {
+#      source = "yandex-cloud/yandex"
+#    }
 #  }
+#  required_version = ">= 0.13"
 #}
 provider "yandex" {
 service_account_key_file = var.service_account_key_file
