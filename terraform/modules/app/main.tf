2ab00fcd5682284d55f0bd45a10a764f61df528c
m4x58 <mvidineev@yandex.ru>
2023-10-10 11:53:30 +0300

    Update main.tf

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = ">= 0.13"
-}
 resource "yandex_compute_instance" "app" {
   name = "reddit-app"
   labels = {
@@ -23,11 +15,7 @@ resource "yandex_compute_instance" "app" {
     }
   }
 
-  scheduling_policy {
-    preemptible = true
-  }
-
-  network_interface {
+ network_interface {
     subnet_id = var.subnet_id
     nat       = true
   }
@@ -35,22 +23,4 @@ resource "yandex_compute_instance" "app" {
   metadata = {
     ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
-
-
-  connection {
-    type        = "ssh"
-    host        = self.network_interface.0.nat_ip_address
-    user        = "ubuntu"
-    agent       = false
-    private_key = file(var.private_key_path)
-  }
-
-provisioner "file" {
-  source      = "/home/ubuntu/git/m4x58_infra/terraform/files/deploy.sh"
-  destination = "/tmp/deploy.sh"
-}
-provisioner "remote-exec" {
-  inline = concat(["echo Provisioning"], [for command in ["chmod +x /tmp/deploy.sh", "/tmp/deploy.sh"]: command if var.provision])
-}
-
 }
