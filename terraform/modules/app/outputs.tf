6afe41ba7ded9c4f107cfeb394bc3118eaf35ed4
m4x58 <mvidineev@yandex.ru>
2023-10-10 10:50:03 +0300

    out

--- terraform/modules/app/outputs.tf
+++ terraform/modules/app/outputs.tf
@@ -1,16 +1,7 @@
-output "external_ip_address_app" {
+/*output "external_ip_address_app" {
   value = yandex_compute_instance.app.network_interface.0.nat_ip_address
+}*/
+
+output "external_ip_address_app" {
+  value = yandex_compute_instance.app.network_interface[0].nat_ip_address
 }
-
-
-### Ansible inventory file
-#resource "local_file" "AnsibleInventory" {
- # content = templatefile("../files/inventory.tmpl",
-  # {
-  #   ip_address_app = module.app.external_ip_address_app
-  #   db_host = module.db.internal_ip_address_db
-  #   ip_address_db = module.db.external_ip_address_db
- #  }
- #)
- # filename = "../../ansible/environments/prod/inventory.yml"
-#}
